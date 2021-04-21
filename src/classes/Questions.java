c6e34eef755f0f8c6ebbb3a6c81a9d8d0f3a5d98
Alberto <alberto.garcia@preving.com>
2021-04-21 08:22:47 +0200

    first commit

---
+++ src/classes/Questions.java
@@ -0,0 +1,58 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Questions {
+
+    //Question 1
+
+
+
+
+
+    //Question 2
+    public  void question2(int[] array){
+
+
+
+    }
+
+    //Question 3
+    public  void question3(int[] array){
+
+
+
+    }
+
+    //Question 5
+
+
+
+    //Question 6
+
+    public void question6(){
+
+
+    }
+
+    //question 7
+
+
+    public void question7(){
+
+    }
+
+
+    public void question8(){
+
+
+    }
+
+
+    public void question9(){
+
+    }
+            
+
+}
