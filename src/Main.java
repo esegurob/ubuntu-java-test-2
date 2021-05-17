b005d1ce160de2ed88fa642377fe7be2fbb33601
eseguro <e.seguro@preving.com>
2021-05-17 12:49:14 +0200

    ADDED: Java app

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-       //todo 10. Create the calls in the Main class for every method created in the Questions class
-
-
-
     }
-
 }
