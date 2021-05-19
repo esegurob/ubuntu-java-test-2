7269d44eb7cad46a7f68d5ff0c61446f2f8fe62a
eseguro <e.seguro@preving.com>
2021-05-19 12:02:33 +0200

    ADDED: Exercises

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,34 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        // Exercise 1
+        // Declare two int variables (X and X)
+        // Calculate and show in the console the sum, subtraction, multiplication and division
+
+
+
+        // Exercise 2
+        // Declare a String with the value "Ubuntu: I am because we are"
+        // Show in the console the number of characters and separate by words
+
+
+
+        // Exercise 3
+        // Declare a 12 numbers array and do the follow points
+        // Show in the console which number is the max and the min.
+        // Show the reverse order of the array
+
+
+
+        // Exercise 4
+
+
+
+        // Exercise 5
+        // Declare a String with the value "Ubuntu: I am because we are"
+        // Show in the console how many vowels the string have
+        // Replace the spaces for the character '-'
+
     }
 }
