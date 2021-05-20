c2de46f98fbfa8192467ed5b97f88ce4de440493
eseguro <e.seguro@preving.com>
2021-05-20 10:23:01 +0200

    UPDATED: Exercises changed

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,43 @@ public class Main {
 
 
         // Exercise 1
-        // Declare two int variables (X and X)
-        // Calculate and show in the console the sum, subtraction, multiplication and division
-
+        // Create two numbers (int) variables (12 and 7)
+        // Calculate and print the sum
+        // Calculate and print the subtraction
+        // Calculate and print the multiplication
+        // Calculate and print the division
 
 
         // Exercise 2
-        // Declare a String with the value "Ubuntu: I am because we are"
-        // Show in the console the number of characters and separate by words
+        // Create a string with the value “Ubuntu: I am because we are”
+        // Show in the console the number of characters
+        // Show the string separated by words
+        // Replace the spaces for the character ‘-’
 
 
 
         // Exercise 3
-        // Declare a 12 numbers array and do the follow points
-        // Show in the console which number is the max and the min.
+        // Create a 6 number array, and do the following
+        // Show in the console which number is the highest
+        // Show in the console which number is the lowest
         // Show the reverse order of the array
 
 
 
         // Exercise 4
+        // Create a variable date and initialize it with your birthday
+        // Calculate your age
+        // Print the date and the age
 
 
 
         // Exercise 5
-        // Declare a String with the value "Ubuntu: I am because we are"
-        // Show in the console how many vowels the string have
-        // Replace the spaces for the character '-'
+        // Create a 6 number array, and do the following
+        // Print the average
+        // Check if it is approved or not (more than 5)
+        // Check number by number if are approved or not (more than 5)
+
+
 
     }
 }
