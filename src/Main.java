bff78729b875d4826c1ad08fc19fac69f0b862f3
eseguro <e.seguro@preving.com>
2021-05-26 11:34:42 +0200

    UPDATED: Change some exercises for David, who's doing the exam another day

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
         System.out.println("Exercise 1 ----------------------------");
         // Exercise 1
-        // Create two numbers (int) variables (12 and 7)
-        // Calculate and print the sum
-        // Calculate and print the subtraction
+        // Create two numbers (int) variables (14 and 6)
+        // Show by console which one is the highest or if they are equal
         // Calculate and print the multiplication
         // Calculate and print the division
 
@@ -19,8 +18,7 @@ public class Main {
         // Exercise 2
         // Create a string with the value “Ubuntu: I am because we are”
         // Show in the console the number of characters
-        // Show the string separated by words
-        // Replace the spaces for the character ‘-’
+        // Replace the spaces for the character ‘*’ and show the result in the console
 
 
 
@@ -30,8 +28,7 @@ public class Main {
         System.out.println("Exercise 3 ----------------------------");
         // Exercise 3
         // Create a 6 number array, and do the following
-        // Show in the console which number is the highest
-        // Show in the console which number is the lowest
+        // Show by console the odd numbers
         // Show the reverse order of the array
 
 
@@ -41,9 +38,8 @@ public class Main {
 
         System.out.println("Exercise 4 ----------------------------");
         // Exercise 4
-        // Create a variable date and initialize it with your birthday
-        // Calculate your age
-        // Print the date and the age
+        // Create a variable date and show the date and time from now.
+        // Sum three days at the date and show by console if it is weekend or not
 
 
 
