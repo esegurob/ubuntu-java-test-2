9c2d7f688cd454d51538462cabd6d68efdcd244f
eseguro <e.seguro@preving.com>
2021-06-04 08:33:45 +0200

    UPDATED: Added the exercises in the main class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,85 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        System.out.println("Exercise 1 ----------------------------");
+//        Exercise 1:
+//        Create the following domain in the classes Folder. The class must have:
+//        The getters and setters methods
+//        Two constructors, one of them empty and the other with all the attributes
+//        The to string method
+
+//        Class name: Brand
+//        Attributes:
+//          int id;
+//          String model;
+//          Date makeYear;
+//          int speed;
+//          int price;
+//          boolean available;
+
+
+
+
+        System.out.println("/--------------------------------------");
+
+        System.out.println("Exercise 2 ----------------------------");
+//        Exercise 2
+//        Create the following domain in the classes folder. The class must have:
+//        The getters and setters methods
+//        Two constructors, one of them empty and the other with all the attributes
+//        The to string method
+
+//        Class name: Car
+//        Attributes:
+//          int id;
+//          Brand brand;
+//          String color;
+
+
+
+
+        System.out.println("/--------------------------------------");
+
+        System.out.println("Exercise 3 ----------------------------");
+//        Exercise 3
+//        Create five objects (instances) of the class CAR (remember that the CAR class have a BRAND class attribute inside) with the following data:
+
+//        Object Car structure:
+//        (id, Brand (id, model, makeYear, speed, available), color)
+//
+//          car1= 1, Brand(1,Mercedes, 1997, 140, yes), “red”;
+//          car2= 2, Brand(2, BMW, 2008, 120, no), “black”;
+//          car3= 3, Brand(3, Mitsubishi, 2020, 130, yes), “white”;
+//          car4= 4, Brand(4, Toyota, 1990, 100, yes), “grey”;
+//          car5= 5, Brand(2, BMW, 2017, 160, yes), “yellow”;
+
+
+
+
+        System.out.println("/--------------------------------------");
+
+        System.out.println("Exercise 4 ----------------------------");
+//        Exercise 4
+//        Create a List of the Car domain. Add the Car objects already created in exercise 3 to the List and print them in the console.
+
+
+
+
+
+
+        System.out.println("/--------------------------------------");
+
+        System.out.println("Exercise 5 ----------------------------");
+//        Exercise 4
+//        Create a List<Car> usar la misma and apply the following functionalities:
+
+//          - Print all the cars with the available attribute is ‘true’
+//          - Print all the cars in order by price (asc)
+//          - Print all the cars with brand name is BMW
+//          - Set the Toyota brand color attribute to blue and print in the console
+
+
+
     }
 }
